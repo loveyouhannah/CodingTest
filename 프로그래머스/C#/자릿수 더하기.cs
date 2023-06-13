08d8f1ab87b6ea171d16fd218ef0ee6e30d2e1b2
Hannah <cheeze2017@naver.com>
2023-06-13 22:02:29 +0900

    프로그래머스 C# 2문제

---
+++ 프로그래머스/C#/자릿수 더하기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int n) 
+    {
+        int answer = 0;
+
+        while(n > 0)
+        {
+            answer += n % 10;
+            n /= 10;
+        }
+
+        return answer;
+    }
+}
