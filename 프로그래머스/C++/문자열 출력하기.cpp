41b57ff724bca9af85797366cb3bd7beaf5bbfbf
Hannah <cheeze2017@naver.com>
2023-07-14 19:25:28 +0900

    프로그래머스 C++ 1문제

---
+++ 프로그래머스/C++/문자열 출력하기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    cout << str;
+    return 0;
+}
+
+//이 문제는 cout을 알려주기 위함
+	//return 0; 필요하지 않음.
+	//cout << str;대신 cout<<str<<endl;과 cout << str << '\n';을 사용가능.
+	
