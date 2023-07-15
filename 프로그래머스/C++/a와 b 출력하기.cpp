226b50fdd75baaf9d830dda1f3c6a57bae7e15a6
Hannah <cheeze2017@naver.com>
2023-07-15 20:44:46 +0900

    프로그래머스 C++ 1문제

---
+++ 프로그래머스/C++/a와 b 출력하기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+	
+	//start 
+    cout << "a = " << a << endl;
+    cout << "b = " << b;
+    //end
+	
+	return 0;
+}
+
+//cout의 활용법에 대한 이해를 확인하는 문제
